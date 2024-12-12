c642281e4eef94716c43d87a8068d718229fb7e9
Qile Lee <1750105862@qq.com>
2024-12-13 00:17:00 +0800

    style 修改文档样式

--- src/scripts/checkEnd.js
+++ src/scripts/checkEnd.js
@@ -4,9 +4,9 @@ const checkEnd = (stateMatrix,clickEventFun) => {
     for(let i = 0; i < stateMatrix.length; i++) {
         for(let j = 0; j < stateMatrix[i].length; j++) {
             const curEle = stateMatrix[i][j];
-            if(!curEle) continue;
-            if(stateMatrix[i][j + 1] && stateMatrix[i][j + 1].value === curEle.value) return;
-            if(stateMatrix[i + 1] && stateMatrix[i + 1][j].value === curEle.value) return; 
+            if(curEle == null) continue;
+            if(stateMatrix[i][j + 1] == undefined && stateMatrix[i][j + 1].value === curEle.value) return;
+            if(stateMatrix[i + 1] == undefined && stateMatrix[i + 1][j].value === curEle.value) return; 
         }
     }
     cover.style.display = 'flex';
