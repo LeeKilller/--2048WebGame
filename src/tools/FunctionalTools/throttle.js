4d4f15b984564a77928617b5f23a9cee0ea20d1c
Qile Lee <1750105862@qq.com>
2024-12-13 12:30:11 +0800

    feat 增添节流条件

--- src/tools/FunctionalTools/throttle.js
+++ src/tools/FunctionalTools/throttle.js
@@ -4,8 +4,9 @@ import { gloablVar } from "../../scripts/global";
 const throttle = (fun,delay = 200) => {
     let oldTime = Date.now();
     return (...args) => {
+        //console.log(gloablVar);
         const newTime = Date.now();
-        if(newTime - oldTime > delay) {
+        if(newTime - oldTime > delay && !gloablVar.hasItemMoved) {
             fun(...args);
             oldTime = newTime;
         }
